200f93dcca6fcc9136f6946d56a21f1bc8c42d31
isHardCoded <ishardcoded@gmail.com>
2026-02-13 19:02:49 +0300

    feat: реализация multicast

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -4,98 +4,88 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Threads
 {
-    class StateObject
-    {
-        public const int BufferSize = 1024;
-        public byte[] Buffer = new byte[BufferSize];
-        public Socket Socket;
-        public EndPoint RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
-    }
-
-    class UdpServer
-    {
-        private Socket _socket;
-        private StateObject _state;
-        private int _port;
-
-        public UdpServer(int port)
-        {
-            _port = port;
-            _state = new StateObject();
-        }
-
-        public void Start()
-        {
-            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            _socket.Bind(new IPEndPoint(IPAddress.Any, _port));
-            _state.Socket = _socket;
-
-            Console.WriteLine($"UDP сервер запущен на порту {_port}");
-            Console.WriteLine("Ожидание сообщений...");
-
-            BeginReceive();
-        }
-
-        private void BeginReceive()
-        {
-            _socket.BeginReceiveFrom(_state.Buffer, 0, StateObject.BufferSize, SocketFlags.None, ref _state.RemoteEndPoint, ReceiveCompleted, _state);
-        }
-
-        private void ReceiveCompleted(IAsyncResult asyncResult)
-        {
-            StateObject stateObject = (StateObject)asyncResult.AsyncState;
-            int bytesRead = stateObject.Socket.EndReceiveFrom(asyncResult, ref stateObject.RemoteEndPoint);
-
-            string message = Encoding.UTF8.GetString(stateObject.Buffer, 0, bytesRead);
-            Console.WriteLine($"Получено от {stateObject.RemoteEndPoint}");
-            Console.WriteLine(message);
-
-            BeginReceive();
-        }
-    }
-
-    class UdpClientSender
-    {
-        private UdpClient _udpClient;
-
-        public UdpClientSender()
-        {
-            _udpClient = new UdpClient();
-        }
-
-        public void Send(string message, string ip, int port)
-        {
-            byte[] data = Encoding.UTF8.GetBytes(message);
-            _udpClient.Send(data, data.Length, ip, port);
-        }
-    }
-
     internal class Program
     {
+        private const string MULTICAST_IP = "224.5.5.5";
+        private const int PORT = 4567;
+        private static int INTERVAL = 1000;
+        private static string message = "";
+
+        static void Listener()
+        {
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
+                socket.Bind(endPoint);
+
+                IPAddress multicastAddr = IPAddress.Parse(MULTICAST_IP);
+
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastAddr, IPAddress.Any));
+
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    int bytesRead = socket.Receive(buffer);
+                    string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    Console.WriteLine("Получено: " + received);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void SendMessage()
+        {
+            try
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+
+                IPAddress dest = IPAddress.Parse(MULTICAST_IP);
+                IPEndPoint endPoint = new IPEndPoint(dest, PORT);
+
+                socket.Connect(endPoint);
+
+                socket.Send(Encoding.UTF8.GetBytes(message));
+
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
         static void Main(string[] args)
         {
-            int port = 100;
-            UdpServer server = new UdpServer(port);
+            Thread listenerThread = new Thread(Listener);
+            listenerThread.IsBackground = true;
+            listenerThread.Start();
 
-            server.Start();
-
-            UdpClientSender client = new UdpClientSender();
-            Console.Write("Введите сообщение для отправки по UDP: ");
+            Console.WriteLine("Multicast чат запущен.");
+            Console.Write("Введите сообщение: ");
 
             while (true)
             {
-                string message = Console.ReadLine();
-                
-                if (string.IsNullOrEmpty(message))
-                {
-                    break;
-                }
+                string input = Console.ReadLine();
 
-                client.Send(message, "127.0.0.1", port);
-                Console.WriteLine("Сообщение отправлено");
+                if (!string.IsNullOrEmpty(input))
+                {
+                    message = input;
+                    Thread senderThread = new Thread(SendMessage);
+                    senderThread.IsBackground = true;
+                    senderThread.Start();
+                }
             }
         }
     }
---
+++ Threads/async-sockets/UdpServer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads.async_sockets
+{
+    internal class StateObject
+    {
+        public const int BufferSize = 1024;
+        public byte[] Buffer = new byte[BufferSize];
+        public Socket Socket;
+        public EndPoint RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    }
+
+    class UdpServer
+    {
+        private Socket _socket;
+        private StateObject _state;
+        private int _port;
+
+        public UdpServer(int port)
+        {
+            _port = port;
+            _state = new StateObject();
+        }
+
+        public void Start()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.Bind(new IPEndPoint(IPAddress.Any, _port));
+            _state.Socket = _socket;
+
+            Console.WriteLine($"UDP сервер запущен на порту {_port}");
+            Console.WriteLine("Ожидание сообщений...");
+
+            BeginReceive();
+        }
+
+        private void BeginReceive()
+        {
+            _socket.BeginReceiveFrom(_state.Buffer, 0, StateObject.BufferSize, SocketFlags.None, ref _state.RemoteEndPoint, ReceiveCompleted, _state);
+        }
+
+        private void ReceiveCompleted(IAsyncResult asyncResult)
+        {
+            StateObject stateObject = (StateObject)asyncResult.AsyncState;
+            int bytesRead = stateObject.Socket.EndReceiveFrom(asyncResult, ref stateObject.RemoteEndPoint);
+
+            string message = Encoding.UTF8.GetString(stateObject.Buffer, 0, bytesRead);
+            Console.WriteLine($"Получено от {stateObject.RemoteEndPoint}");
+            Console.WriteLine(message);
+
+            BeginReceive();
+        }
+    }
+
+    class UdpClientSender
+    {
+        private UdpClient _udpClient;
+
+        public UdpClientSender()
+        {
+            _udpClient = new UdpClient();
+        }
+
+        public void Send(string message, string ip, int port)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            _udpClient.Send(data, data.Length, ip, port);
+        }
+
+    public void Run()
+        {
+            int port = 100;
+            UdpServer server = new UdpServer(port);
+
+            server.Start();
+
+            UdpClientSender client = new UdpClientSender();
+            Console.Write("Введите сообщение для отправки по UDP: ");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(message))
+                {
+                    break;
+                }
+
+                client.Send(message, "127.0.0.1", port);
+                Console.WriteLine("Сообщение отправлено");
+            }
+        }
+    }
+}
