0df5698919962d4dccc5e6716d14138a81d11147
isHardCoded <ishardcoded@gmail.com>
2026-02-17 19:44:43 +0300

    Работа с SMTP

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -5,87 +5,39 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Net.Mail;
 
 namespace Threads
 {
     internal class Program
     {
-        private const string MULTICAST_IP = "224.5.5.5";
-        private const int PORT = 4567;
-        private static int INTERVAL = 1000;
-        private static string message = "";
-
-        static void Listener()
-        {
-            try
-            {
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
-                socket.Bind(endPoint);
-
-                IPAddress multicastAddr = IPAddress.Parse(MULTICAST_IP);
-
-                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(multicastAddr, IPAddress.Any));
-
-                byte[] buffer = new byte[1024];
-
-                while (true)
-                {
-                    int bytesRead = socket.Receive(buffer);
-                    string received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-
-                    Console.WriteLine("Получено: " + received);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
-        static void SendMessage()
-        {
-            try
-            {
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-                socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
-
-                IPAddress dest = IPAddress.Parse(MULTICAST_IP);
-                IPEndPoint endPoint = new IPEndPoint(dest, PORT);
-
-                socket.Connect(endPoint);
-
-                socket.Send(Encoding.UTF8.GetBytes(message));
-
-                socket.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-        }
-
         static void Main(string[] args)
         {
-            Thread listenerThread = new Thread(Listener);
-            listenerThread.IsBackground = true;
-            listenerThread.Start();
-
-            Console.WriteLine("Multicast чат запущен.");
-            Console.Write("Введите сообщение: ");
-
-            while (true)
+            try
             {
-                string input = Console.ReadLine();
+                string to = "z1tra@yandex.ru";
+                string from = "z1tra@yandex.com";
 
-                if (!string.IsNullOrEmpty(input))
-                {
-                    message = input;
-                    Thread senderThread = new Thread(SendMessage);
-                    senderThread.IsBackground = true;
-                    senderThread.Start();
-                }
+                string password = "pduwuvdlbzbahdye";
+
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(from, "ishardcoded");
+                message.To.Add(to);
+                message.Subject = "Тестовое письмо";
+                message.Body = "Привет, как дела? \n\nЭто письмо отправлено из C# приложения";
+                message.IsBodyHtml = false;
+
+                SmtpClient smpt = new SmtpClient("smtp.yandex.ru", 587);
+                smpt.Credentials = new NetworkCredential(from, password);
+                smpt.EnableSsl = true;
+
+                smpt.Send(message);
+
+                Console.WriteLine("Письмо отправленно успешно!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
         }
     }
